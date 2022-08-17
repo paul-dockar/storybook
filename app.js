9eb0ed00f782d39d314bdc88f657e78c86b8936d
Paul Dockar <pdockar@gmail.com>
2022-08-17 19:05:54 +1000

    added edit story page- in progress

--- app.js
+++ app.js
@@ -27,8 +27,18 @@ if (process.env.NODE_ENV === `development`) {
     app.use(morgan('dev'));
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
+
 // Handlebars
-app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+    '.hbs',
+    engine({
+        helpers: { formatDate, stripTags, truncate, editIcon, select },
+        defaultLayout: 'main',
+        extname: '.hbs',
+    })
+);
 app.set('view engine', '.hbs');
 
 // Session
@@ -44,6 +54,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+});
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
