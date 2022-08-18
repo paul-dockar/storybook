00f702fbc47031d081695819b6923fdf4762e3a3
Paul Dockar <pdockar@gmail.com>
2022-08-18 15:18:45 +1000

    fixed bug with status enum on edit story page

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -31,7 +31,7 @@ module.exports = {
     select: function (selected, options) {
         return options
             .fn(this)
-            .replace(new RegExp(' value="' + selected + '"'), '$& selected="selected"')
-            .replace(new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
+            .replace(' selected', '')
+            .replace(`option value='${selected}'`, `$& selected`);
     },
 };
